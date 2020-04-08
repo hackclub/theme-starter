39a2ab11af856351d018cd9f77a0367b56d30253
Lachlan Campbell <lachlan@hackclub.com>
2020-04-08 14:07:53 -0400

    Begin theme starter

---
+++ pages/_app.js
@@ -0,0 +1,19 @@
+import * as React from 'react'
+import NextApp from 'next/app'
+
+// import '@hackclub/theme/fonts/reg-bold.css'
+import theme from '@hackclub/theme'
+import { ThemeProvider } from 'theme-ui'
+import ColorSwitcher from '../components/color-switcher'
+
+export default class App extends NextApp {
+  render() {
+    const { Component, pageProps } = this.props
+    return (
+      <ThemeProvider theme={theme}>
+        <ColorSwitcher />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    )
+  }
+}
---
+++ pages/index.mdx
@@ -0,0 +1,9 @@
+import { Container } from 'theme-ui'
+
+<Container>
+
+# Hello!
+
+You’re using the [Hack Club Theme Starter](https://github.com/hackclub/theme-starter).
+
+</Container>
